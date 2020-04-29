6a4761b5cac83de92293b3eb95a62467bf7012f9
Saad Rahim <saad.rahim@amd.com>
2020-04-29 15:36:23 -0700

    Fixing CI errors

---
+++ .jenkins/common.groovy
@@ -0,0 +1,36 @@
+// This file is for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+def runCompileCommand(platform, project, jobName)
+{
+    project.paths.construct_build_prefix()
+
+    String hipclangArgs = jobName.contains('hipclang') ? '--hip-clang' : ''
+    def getRCCL = auxiliary.getLibrary('rccl',platform.jenkinsLabel,'develop')
+
+    def command = """#!/usr/bin/env bash
+                set -x
+                ${getRCCL}
+                ${auxiliary.exitIfNotSuccess()}
+                cd ${project.paths.project_build_prefix}
+                ${project.paths.build_command}
+                ${auxiliary.exitIfNotSuccess()}
+            """
+
+    platform.runCommand(this,command)
+}
+
+def runTestCommand (platform, project)
+{
+    String sudo = auxiliary.sudo(platform.jenkinsLabel)
+
+    def command = """#!/usr/bin/env bash
+                set -x
+		python3 -m pytest -k "not MPI" --junitxml=./testreport.xml
+            """
+
+   platform.runCommand(this, command)
+   junit "${project.paths.project_build_prefix}/build/release/*.xml"
+}
+
+return this
