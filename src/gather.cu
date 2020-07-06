5851b3f598a16478b49dba631e2dc179542fcd04
Wenkai Du <wenkai.du@amd.com>
2020-07-06 18:11:28 +0000

    Change scatter and gather bandwidth calculation to match alltoall

--- src/gather.cu
+++ src/gather.cu
@@ -64,10 +64,10 @@ testResult_t GatherInitData(struct threadArgs* args, ncclDataType_t type, ncclRe
 }
 
 void GatherGetBw(size_t count, int typesize, double sec, double* algBw, double* busBw, int nranks) {
-  double baseBw = (double)(count * typesize * (nranks - 1)) / 1.0E9 / sec;
+  double baseBw = (double)(count * typesize) / 1.0E9 / sec;
 
   *algBw = baseBw;
-  double factor = 1;
+  double factor = ((double)(nranks-1))/((double)(nranks));
   *busBw = baseBw * factor;
 }
 
@@ -79,7 +79,10 @@ testResult_t GatherRunColl(void* sendbuff, void* recvbuff, size_t count, ncclDat
 
   int rank;
   NCCLCHECK(ncclCommUserRank(comm, &rank));
-#if NCCL_MAJOR >= 2 && NCCL_MINOR >= 7
+#if NCCL_MAJOR < 2 || NCCL_MINOR < 7
+  printf("NCCL 2.7 or later is needed for gather. This test was compiled with %d.%d.\n", NCCL_MAJOR, NCCL_MINOR);
+  return testNcclError;
+#else
 #if defined(RCCL_GATHER_SCATTER) && defined(USE_RCCL_GATHER_SCATTER)
   if (rank == root)
     NCCLCHECK(ncclGather(sendbuff, recvbuff, count, type, root, comm, stream));
@@ -94,8 +97,8 @@ testResult_t GatherRunColl(void* sendbuff, void* recvbuff, size_t count, ncclDat
   NCCLCHECK(ncclSend(sendbuff, count, type, root, comm, stream));
   NCCLCHECK(ncclGroupEnd());
 #endif
-#endif
   return testSuccess;
+#endif
 }
 
 struct testColl gatherTest = {
