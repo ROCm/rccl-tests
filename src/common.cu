dc1ad4853d7ec738387d42a75a58a98d7af00c7b
Wenkai Du <wenkai.du@amd.com>
2021-09-22 08:43:01 -0700

    Fix divide by zero error

--- src/common.cu
+++ src/common.cu
@@ -527,7 +527,7 @@ testResult_t startColl(struct threadArgs* args, ncclDataType_t type, ncclRedOp_t
   size_t count = args->nbytes / wordSize(type);
 
   // Try to change offset for each iteration so that we avoid cache effects and catch race conditions in ptrExchange
-  size_t totalnbytes = max(args->sendBytes, args->expectedBytes);
+  size_t totalnbytes = std::max(args->sendBytes, args->expectedBytes);
   size_t steps = totalnbytes ? args->maxbytes / totalnbytes : 1;
   size_t shift = totalnbytes * (iter % steps);
 
