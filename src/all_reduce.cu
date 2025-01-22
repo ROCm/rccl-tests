9a5d7d2808e3c77719282046b9a3083e36aadc54
nileshnegi <Nilesh.Negi@amd.com>
2025-01-22 14:21:50 -0600

    [AllReduce] Enable FP8 conditions only if FP8 is supported
    Signed-off-by: nileshnegi <Nilesh.Negi@amd.com>

--- src/all_reduce.cu
+++ src/all_reduce.cu
@@ -65,8 +65,10 @@ testResult_t AllReduceRunTest(struct threadArgs* args, int root, ncclDataType_t 
   ncclRedOp_t *run_ops;
   const char **run_typenames, **run_opnames;
   int type_count, op_count;
+#if defined(RCCL_FLOAT8)
   if((type == ncclFp8E4M3 || type == ncclFp8E5M2) && op == ncclProd)
     return testSuccess;
+#endif
 
   if ((int)type != -1) {
     type_count = 1;
@@ -90,8 +92,10 @@ testResult_t AllReduceRunTest(struct threadArgs* args, int root, ncclDataType_t 
 
   for (int i=0; i<type_count; i++) {
     for (int j=0; j<op_count; j++) {
+#if defined(RCCL_FLOAT8)
       if((i == ncclFp8E4M3 || i == ncclFp8E5M2) && j == ncclProd)
         continue;
+#endif
       TESTCHECK(TimeTest(args, run_types[i], run_typenames[i], run_ops[j], run_opnames[j], -1));
     }
   }
